5e25142107e05a2f0aa9a5d0b593a6433f8c5136
Vistiaho Valtteri E T <vistiaho@dat-8.cs.helsinki.fi>
2017-09-08 14:27:56 +0300

    Kivy test

--- project/Main.py
+++ project/Main.py
@@ -1,3 +1,27 @@
+import kivy
+kivy.require('1.1.0')
+
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+
+class MyButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(MyButton, self).__init__(**kwargs)
+
+    def on_press(self):
+        self.source = 'a.jpg'
+
+    def on_release(self):
+
+        self.source = ''
+
+
+class MyApp(App):
+
+    def build(self):
+        return MyButton()
+
+
 if __name__ == '__main__':
-    print("Hello world!")
-    
+    MyApp().run()
