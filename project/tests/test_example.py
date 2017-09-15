8bd8bd3fa02e8d10728add48995418f4cdd01575
Petri Kallio <kallionpetri@gmail.com>
2017-09-15 13:23:54 +0300

    Kivy testing test

---
+++ project/tests/test_example.py
@@ -0,0 +1,17 @@
+import pytest
+from project.tests.tools import simulate
+
+from project.tests.simulator_fixture import simulator
+
+@pytest.mark.parametrize("params", [{}])
+@simulate
+def test_example(simulator):
+
+    # widgets are selected with xpath
+    simulator.assert_count("//SwitchLayout//Button", 2)
+
+    # deep tree goes reversed through the tree
+    simulator.assert_text("//SwitchLayout//Button[1]", "Master")
+    simulator.assert_text("//SwitchLayout//Button[2]", "Slave")
+
+    simulator.tap("//SwitchLayout//Button[1]")
---
+++ project/tests/tools.py
@@ -0,0 +1,33 @@
+from functools import partial
+
+from kivy.clock import Clock
+
+
+def to_task(s):
+    s.press("//MenuButtonTitled[@name='LOGO']")
+    s.assert_on_screen('activity')
+    s.press('//StartNowButton')
+    s.assert_on_screen('tasks')
+    s.tap("//TestIntro//TestCarouselForwardButton")
+    s.assert_on_screen("test", manager_selector="//TasksScreen/ScreenManager")
+    s.tap("//BlinkImageButton[@name='task_icon']")
+
+
+def without_schedule_seconds(function):
+    def inner(*args, **kwargs):
+        function(*args[:-1], **kwargs)
+
+    return inner
+
+def simulate(function):
+    def simulate_inner(simulator, params):
+        simulator.start(function, params or {})
+    return simulate_inner
+
+
+def execution_step(function):
+    def execution_step_inner(self, *args, **kwargs):
+        self.execution_queue.append((function, args, kwargs))
+
+    return execution_step_inner
+
