8b2a68c93eb82b33eda60a59dd3bf93f58a8b55a
Petri Kallio <kallionpetri@gmail.com>
2017-09-22 13:55:46 +0300

    fix to test_main.py

--- project/tests/test_Main.py
+++ project/tests/test_Main.py
@@ -4,27 +4,31 @@ import os
 
 class MainTest(unittest.TestCase):
     timeout_in_seconds = 2
+    main_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    main_file = "Main.py"
+
+    def get_command_with_arguments(self, argv):
+        command = "timeout --preserve-status -sSIGTERM "
+        command += str(self.timeout_in_seconds) + "s python3 "
+        command += os.path.join(self.main_path, self.main_file)
+
+        for arg in argv:
+            command += " " + str(arg)
+
+        return command
 
     def test_without_parameters(self):
-        return_value = os.system("timeout --preserve-status -sSIGTERM " +
-                                 str(self.timeout_in_seconds) +
-                                 "s python3 ~/Remu/project/Main.py")
+        return_value = os.system(self.get_command_with_arguments([]))
         self.assertEqual(0, return_value)
 
     def test_with_one_parameter(self):
-        return_value = os.system("timeout --preserve-status -sSIGTERM " +
-                                 str(self.timeout_in_seconds) +
-                                 "s python3 ~/Remu/project/Main.py master")
+        return_value = os.system(self.get_command_with_arguments(["master"]))
         self.assertEqual(0, return_value)
 
     def test_with_two_parameters(self):
-        return_value = os.system("timeout --preserve-status -sSIGTERM " +
-                                 str(self.timeout_in_seconds) +
-                                 "s python3 ~/Remu/project/Main.py master 0.0.0.0")
+        return_value = os.system(self.get_command_with_arguments(["master" "0.0.0.0"]))
         self.assertEqual(0, return_value)
 
     def test_with_three_parameters(self):
-        return_value = os.system("timeout --preserve-status -sSIGTERM " +
-                                 str(self.timeout_in_seconds) +
-                                 "s python3 ~/Remu/project/Main.py master 0.0.0.0 too_much_params")
+        return_value = os.system(self.get_command_with_arguments(["master" "0.0.0.0", "too_many_args"]))
         self.assertEqual(0, return_value)
