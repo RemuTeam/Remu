6a61c184ecab3396cb65b5047b53af7014b81448
Jani Takkinen <jani.takkinen@cs.helsinki.fi>
2017-09-22 15:28:33 +0300

    removed connection test

--- project/RemuTCP/RemuTCP.py
+++ project/RemuTCP/RemuTCP.py
@@ -64,23 +64,23 @@ class RemuTCP:
     address = None
     port = None
 
-    def __init__(self, app, master=False, address=None):
+    def __init__(self, app, master=False, address=None, port=8000):
         self.app = app
         if master:
             self.address = address
-            self.connect_to_slave()
+            self.connect_to_slave(port)
         else:
-            self.listen_to_master()
+            self.listen_to_master(port)
 
     def stop_listening(self):
         self.port.stopListening()
 
-    def connect_to_slave(self):
-        reactor.connectTCP(self.address, 8000, RemuProtocolFactory(self))
+    def connect_to_slave(self, port):
+        reactor.connectTCP(self.address, port, RemuProtocolFactory(self))
 
-    def listen_to_master(self):
+    def listen_to_master(self, port):
         print("listening")
-        self.port = reactor.listenTCP(8000, RemuProtocolFactory(self))
+        self.port = reactor.listenTCP(port, RemuProtocolFactory(self))
 
     def on_connection(self, connection):
         print("Connected successfully!")
--- project/tests/RemuTCP/test_RemuTCP.py
+++ project/tests/RemuTCP/test_RemuTCP.py
@@ -1,27 +1,19 @@
 import unittest
+import time
 from RemuTCP.RemuTCP import RemuTCP
 
 class TestRemuTCPMethods(unittest.TestCase):
 
-    def test_init_no_params(self):
-        tcp = RemuTCP(None)
+    def test_init_slave(self):
+        tcp = RemuTCP(None, False, None, 8001)
         self.assertIsNone(tcp.address)
         self.assertIsNotNone(tcp.port)
         tcp.stop_listening()
 
     def test_init_master(self):
-        tcp = RemuTCP(None, True, "127.0.0.1")
+        tcp = RemuTCP(None, True, "127.0.0.1", 8002)
         self.assertEqual("127.0.0.1", tcp.address)
         self.assertIsNone(tcp.port)
-
-    def test_connection_successful(self):
-        slave = RemuTCP(None)
-        master = RemuTCP(None, True, "127.0.0.1")
-        self.assertIsNotNone(slave.connection)
-        self.assertIsNotNone(master.connection)
-        slave.stop_listening()
-
-
 
 
 
