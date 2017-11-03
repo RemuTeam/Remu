11ee99cb559053d96b0589eb71126dd3305ed99e
Jani Takkinen <jani.takkinen@cs.helsinki.fi>
2017-11-03 13:20:26 +0200

    Presentation element created

---
+++ project/Domain/PresentationElement.py
@@ -0,0 +1,18 @@
+
+class PresentationElement():
+
+    def __init__(self, type, source):
+        self.type = type
+        self.source_file = source
+        self.content = None
+        self.prepare_content()
+
+    def prepare_content(self):
+        if self.type == 'text':
+            with open(self.source_file) as file:
+                self.content = file.readlines()
+        else:
+            self.content = self.source_file
+
+    def get_content(self):
+        return self.content
