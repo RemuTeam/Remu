513add972e779a2089b371d2c34826baccd3e1a4
Vistiaho Valtteri E T <vistiaho@dat-5.cs.helsinki.fi>
2017-11-30 12:39:55 +0200

    MasterGUILayout actually represents the situation in the slave's presentation

--- project/GUI/MasterGUI/SlavePresentation.py
+++ project/GUI/MasterGUI/SlavePresentation.py
@@ -47,22 +47,17 @@ class SlavePresentation(StackLayout):
         presentation_content = []
         for i in range(len(self.children)):
             presentation_content.append(self.children[i].visual_name)
-        return presentation_content[::-1]
+        self.presentation_data.set_files(presentation_content[::-1])
+        return self.presentation_data
 
     def visualize_next(self):
         """
         Highlights the next visual, indicating it is the currently active visual
         """
-        if self.current_active == -1:
-            self.current_active += 1
-            return -1
-        self.visuals[self.current_active - 1].set_inactive()
-        if self.current_active == len(self.visuals):
-            self.current_active = -1
-        if -1 < self.current_active < len(self.visuals):
-            self.visuals[self.current_active].set_active()
-            self.current_active += 1
-        return self.current_active
+        for visual in self.visuals:
+            visual.set_inactive()
+        self.visuals[self.presentation_data.index].set_active() if self.presentation_data.index != -1 else None
+        return self.presentation_data.index
 
     def reset(self):
         self.current_active = 0
