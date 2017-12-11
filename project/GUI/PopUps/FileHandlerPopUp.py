10e96d558f8a134d4e2db4790a5115e20f081597
Petri Kallio <kallionpetri@gmail.com>
2017-12-11 17:16:38 +0200

    documentation

--- project/GUI/PopUps/FileHandlerPopUp.py
+++ project/GUI/PopUps/FileHandlerPopUp.py
@@ -54,6 +54,11 @@ class FileHandlerPopUp(Popup, EventDispatcher):
         self.button_text = callback_button_text
 
     def set_popup_functionality(self, file_handling_mode):
+        """
+        Sets the visible elements of the popup based on the FileHandlingMode
+        :param file_handling_mode: a FileHandlingMode object
+        :return: None
+        """
         self.ids.filename_input.size_hint = (1, 0.1) if file_handling_mode["save_mode"] else (0, 0)
         self.ids.presentation_list_area.size_hint = (0.3, 1) if file_handling_mode["import_to_presentations"] else (0, 0)
         self.ids.filename_input.opacity = 1 if file_handling_mode["save_mode"] else 0
@@ -61,11 +66,21 @@ class FileHandlerPopUp(Popup, EventDispatcher):
         self.multiselect = file_handling_mode["multiselect"]
 
     def on_dismiss(self):
+        """
+        Called when the popup is dismissed
+        :return: None
+        """
         self.ids.filechooser.unbind(selection=self.check_selections)
         if self.selected_presentations is not None:
             self.unbind(local_presentation_selection=self.check_selections)
 
     def check_selections(self, instance, value):
+        """
+        Called when the selection of the filechooser changes
+        :param instance: not used
+        :param value: not used
+        :return:
+        """
         selection = self.ids.filechooser.selection
         callback_button = self.ids.callback_button
         if len(selection) == 0 or \
@@ -75,18 +90,41 @@ class FileHandlerPopUp(Popup, EventDispatcher):
             callback_button.disabled = False
 
     def filechooser_selection_changed(self, selection):
+        """
+        Called when the selection of the filechooser changes
+        :param selection:
+        :return:
+        """
         if self.file_handling_mode == SaveProject and len(selection) > 0:
             self.ids.text_input.text = get_filename_only(selection[0])
 
     def check_filename(self, instance, value):
+        """
+        Checks that the entered filename is valid
+        :param instance: not used
+        :param value: the value of the text filed in gui
+        :return:
+        """
         self.ids.callback_button.disabled = not check_filename(value)
 
     def populate_presentation_list(self, presentations):
+        """
+        Populates the presentation selection on gui
+        :param presentations: the presentations to add to the gui
+        :return:
+        """
         presentation_list = self.ids.presentation_list
         for p in presentations:
             presentation_list.add_widget(CheckBoxBonanza(p, 0.05, self.on_checkbox_active))
 
     def on_checkbox_active(self, checkbox, value):
+        """
+        A callback to call when a checkbox is checked, adds the selected presentations
+        to the list if checked or removes it if unchecked
+        :param checkbox: the checkbox that was clicked
+        :param value: true if checked, false if unchecked
+        :return:
+        """
         if value:
             self.selected_presentations.append(checkbox.label)
             if self.local_presentation_selection is not None:
